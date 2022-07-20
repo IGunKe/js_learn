0678de092f2453ff979e0d0c37e5f0bfaf5ecafd
IGunKe <xdjyyds@yeah.net>
2022-07-20 20:42:49 +0800

    更新

---
+++ vue3_test/02_src_setup/App.vue
@@ -0,0 +1,48 @@
+<template>
+    <h1>我是APP组件</h1>
+    <h2>{{ name }}</h2>
+    <h2>{{ age }}</h2>
+    <button @click="sayHi">say</button>
+    <br>
+    <button @click="test">vue2读取vue3</button>
+</template>
+
+<script>
+import {h} from 'vue'
+export default {
+    name: 'App',
+    data() {
+        return {
+            sex: '男'
+        }
+    },
+    methods: {
+        test() {
+            //vue2可以读取setup中的数据，反过来不行
+            console.log(this.age);
+        }
+    },
+    setup() {
+        // vue2中的data和方法在vue3中放在setup()
+        //data
+        let name = 'xdj';
+        let age = 12;
+
+        //methods
+        function sayHi() {
+            alert(`我叫${name}, 今年${age}`);
+            console.log(this);
+        }
+        //如果返回一个对象，可以返回数据，方法，
+        return {
+            name,
+            age,
+            sayHi
+        }
+        //如果返回一个渲染函数
+        // return (h) => h(name, age);
+    }
+};
+</script>
+
+<style></style>
---
+++ vue3_test/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+    <h1>我是APP组件</h1>
+    <h2>{{ name }}</h2>
+    <h2>{{ age }}</h2>
+    <h2>{{ job.type }}</h2>
+    <button @click="changeInfo">修改</button>
+</template>
+
+<script>
+import { h } from 'vue';
+import { ref } from 'vue';
+export default {
+    name: 'App',
+    setup() {
+        // vue2中的data和方法在vue3中放在setup()
+        //data
+        let name = ref('xdj');
+        let age = ref(12);
+        let job = ref({
+            type: '前端'
+        });
+        function changeInfo() {
+            name.value = 'qqqq';
+            age.value = 22;
+            //虽然数据改变了但是页面并没有响应式的修改
+            //需要交给ref
+            job.value.type = 'hello';
+            console.log(name, age, job);
+        }
+        //如果返回一个对象，可以返回数据，方法，
+        return {
+            name,
+            age,
+            job,
+            changeInfo
+        };
+    }
+};
+</script>
+
+<style></style>
